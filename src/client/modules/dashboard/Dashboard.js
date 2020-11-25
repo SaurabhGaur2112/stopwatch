f5deac8912868767cadadcaa6448b86bdcc2ea1d
SaurabhGaur68 <gaursaurabh68@gmail.com>
2020-11-25 11:01:07 +0530

    add module css

--- src/client/modules/dashboard/Dashboard.js
+++ src/client/modules/dashboard/Dashboard.js
@@ -7,7 +7,14 @@ class Dashboard extends Component {
   render() {
     return (
       <div className="dashboard">
-        Dashboard
+        <div className="dashboard__content">
+          <div className="dashboard__section">
+            <div className="dashboard__header">
+              <div className="dashboard__header-title">Stopwatch</div>
+            </div>
+            <div className="dashboard__desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+          </div>
+        </div>
       </div>
     );
   }
