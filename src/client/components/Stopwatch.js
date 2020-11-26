ea4ae08ffeb8521976aeb68019963a58d7e106d2
SaurabhGaur68 <gaursaurabh68@gmail.com>
2020-11-26 14:25:23 +0530

    add interval function

--- src/client/components/Stopwatch.js
+++ src/client/components/Stopwatch.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 // react modules
 import Button from '@hawk-ui/button';
 
+let interval;
 function Stopwatch() {
   const [watchState, setWatchState] = useState({
     timePassed: 0,
@@ -11,6 +12,54 @@ function Stopwatch() {
     minutes: '00',
     isStarted: false,
   });
+
+  const formatter = (input) => {
+    const time = input.toString();
+
+    return time.length < 2 ? `0${time}` : time.slice(-2);
+  };
+
+  const formatTime = () => {
+    setWatchState({
+      // ...watchState,
+      milliSeconds: formatter(watchState.timePassed),
+      seconds: formatter(Math.floor((watchState.timePassed / 100) % 60)),
+      minutes: formatter(Math.floor((watchState.timePassed / (100 * 60)) % 60)),
+    });
+    console.log('query watchState', watchState);
+    console.log('query formatter', formatter(watchState.timePassed));
+  };
+
+  const timeNow = () => {
+    const seconds = parseInt(watchState.timePassed, 10) + 1;
+    console.log('query timeNow', seconds);
+    setWatchState({
+      // ...watchState,
+      timePassed: seconds,
+    });
+    formatTime();
+  };
+
+  const startTime = () => {
+    if (watchState.isStarted) {
+      return;
+    }
+    setWatchState({
+      timePassed: 0,
+      milliSeconds: '00',
+      seconds: '00',
+      minutes: '00',
+      isStarted: true,
+    });
+    interval = setInterval(timeNow(), 10);
+  };
+
+  const stopTime = () => {
+    window.clearInterval(interval);
+    setWatchState({
+      isStarted: false,
+    });
+  };
 
   return (
     <div className="stopwatch">
@@ -22,8 +71,16 @@ function Stopwatch() {
         {watchState.milliSeconds}
       </div>
       <div className="stopwatch-button">
-        <Button>Start</Button>
-        <Button>Stop</Button>
+        <Button
+          onClick={() => { startTime(); }}
+        >
+          Start
+        </Button>
+        <Button
+          onClick={() => { stopTime(); }}
+        >
+          Stop
+        </Button>
         <Button>Reset</Button>
       </div>
     </div>
